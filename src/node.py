4191b471c1ffea99bf11f30256b5f9e6a34c3c66
Brett Mattas <bmattas2@gmail.com>
2025-06-09 21:04:15 -0400

    Added Node

---
+++ src/node.py
@@ -0,0 +1,39 @@
+import math
+
+class Node():
+
+    def __init__(self, index: int, x: float, y: float):
+        """Constructor
+
+        Args:
+            index (int): Index within the model
+            x (float): X Coordinate
+            y (float): Y Coordinate
+        """
+        self.index = index
+        self.x = x
+        self.y = y
+
+    def distance(self, anode: 'Node') -> float:
+        """Distance to another node
+
+        Args:
+            anode (Node): Other node
+
+        Returns:
+            float: Distance
+        """
+        return math.sqrt((self.x - anode.x)**2 + (self.y - anode.y)**2)
+    
+    def is_same_location(self, anode: 'Node') -> bool:
+        """Return true if the other node is at the same location
+        uses a hard-coded tolerance.
+
+        Args:
+            anode (Node): Other node
+
+        Returns:
+            bool: True if at the same location
+        """
+        return self.distance(anode) < 1e-6
+    
